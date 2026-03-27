c752c9fda482719044c898a6b8b2e8d481b6368d
roque-eng <roque@edefreitas.com>
2026-03-27 09:59:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
 # ==========================================
 # 🔐 GESTIÓN DE USUARIOS
 # ==========================================
-USUARIOS = {"RDF": "Rockuda.4428", "AB": "ABentancor2025", "GR": "GRobaina2025", "ER": "ERobaina.2025", "EH": "EHugo2025", "GS": "GSanchez2025", "JM": "JMokosce2025", "PG": "PGagliardi2025", "MDF": "MDeFreitas2025", "AC": "ACazarian2025", "MF": "MFlores2025"}
+USUARIOS = {"RDF": "Rockuda.4428", "LT": "LTomasi2025", "AP": "APerdomo2025", "EC": "ECabral2025", "RS": "RSierra2025", "AB": "ABentancor2025", "GR": "GRobaina2025", "ER": "ERobaina.2025", "EH": "EHugo2025", "GS": "GSanchez2025", "JM": "JMokosce2025", "PG": "PGagliardi2025", "MDF": "MDeFreitas2025", "AC": "ACazarian2025", "MF": "MFlores2025"}
 
 if 'logueado' not in st.session_state: st.session_state['logueado'] = False
 if not st.session_state['logueado']:
