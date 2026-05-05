6a772431d7b3282157ec5fa6979cd26481fd289b
roque-eng <roque@edefreitas.com>
2026-05-05 12:51:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -156,13 +156,13 @@ with tab_cot:
     
     col_a, col_b = st.columns(2)
     with col_a:
-        t_ben = "• Auxilio mecánico 24hs: Todas las aseguradoras\n• Cristales: BSE/SBI USD 200, SURA USD 100, MAPFRE ílimitado, SANCOR USD 300\n• Granizo: PORTO sin deducible\nCosto Anual Apartamentos: USD 120\nCosto Anual Casas: USD 190"
+        t_ben = "• Auxilio mecánico 24hs: Todas las aseguradoras\n• Cristales: BSE/SBI USD 200, SURA USD 100, MAPFRE ílimitado, SANCOR USD 300"
         b_cot = st.text_area("Beneficios:", value=t_ben, height=200)
     with col_b:
-        t_h = "• Incendio Edificio: USD 100.000\n• Incendio Contenido: USD 50.000\n• Hurto Contenido: USD 5.000\n• Remoción de Escombros: USD 5.000"
+        t_h = "• Incendio Edificio: USD 100.000\n• Incendio Contenido: USD 50.000\n• Hurto Contenido: USD 5.000\n• Remoción de Escombros: USD 5.000\nCosto Anual Apartamentos: USD 120\nCosto Anual Casas: USD 190"
         c_h = st.text_area("Hogar:", value=t_h, height=130)
-        c_a = st.text_area("Alquiler:", value="• Auto cortesía 15 días\nCosto: UYU 3.500", height=70)
-        c_b = st.text_area("Bici:", value="• Hurto USD 1.000\nCosto: USD 110", height=70)
+        c_a = st.text_area("Alquiler:", value="• Auto cortesía 15 días en caso de que tu vehículo choque y vaya al taller\nCosto: UYU 3.500", height=70)
+        c_b = st.text_area("Bici:", value="• Hurto USD 1.000\n• Daños a Terceros: USD 10.000\nCosto: USD 110", height=70)
 
     # LÓGICA DE BOTÓN ÚNICO
     datos_i = {"n": n_cot, "v": v_cot, "e": e_cot, "cont": cont_cot, "tab": t_edit.to_dict(orient='records'), "ben": b_cot, "ch": c_h, "ca": c_a, "cb": c_b}
